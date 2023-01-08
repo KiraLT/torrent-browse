690ee4876dcb228e0a7113b1cdfa64d5152672c0
KiraLT <KiraLT@users.noreply.github.com>
2023-01-08 22:18:52 +0200

    fix: remove JsSoup dependency

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './services/requests'
 export * from './services/torrent'
-export * from './services/dom'
 export * from './provider'
 export * from './actions'
 export * from './providers'
+
+export { parseHTML, IsomorphicHTMLElement } from 'isomorphic-htmlparser'
